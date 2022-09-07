d2fd1d73ce3875622cc4b4003c0e4d241bbf9331
GeovaniSS <geosilvasousa@gmail.com>
2022-09-07 00:30:29 -0300

    homeController and loginController

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,3 @@
-exports.paginaInicial = (req, res) => {
-  res.render('index', {
-    titulo: 'Um título qualquer',
-    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  })
+exports.index = (req, res) => {
+  return res.render('index')
 } 
-
-exports.trataPost = (req, res) => {
-  res.send(`<h1> Seja bem-vindo ${req.body.nome} </h1>`)
-}
