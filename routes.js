5fbecf8ca77569dfcb756c6d0cdadf277f5d1b00
GeovaniSS <geosilvasousa@gmail.com>
2022-09-07 23:45:46 -0300

    login and register routes

--- routes.js
+++ routes.js
@@ -2,13 +2,17 @@ const express = require('express')
 const route = express.Router() 
 const homeController = require('./src/controllers/homeController') 
 const loginController = require('./src/controllers/loginController')
+const registerController = require('./src/controllers/registerController')
 
 // Rotas da Home
 route.get('/', homeController.index)
 
 // Rotas de Login
 route.get('/login', loginController.index)
-// route.post('/login')
+route.post('/login', loginController.login)
 
+// Rotas de Cadastro
+route.get('/register', registerController.index)
+route.post('/register', registerController.register)
 
 module.exports = route
