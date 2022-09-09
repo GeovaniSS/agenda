f38893f0d51e579c14994d1bc13f3985e2ccc225
GeovaniSS <geosilvasousa@gmail.com>
2022-09-08 23:53:52 -0300

    redirect logged in user to home route | handle GET and POST requests to login and logout route

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,33 @@
+const Login = require('../models/LoginModel')
+
 exports.index = (req, res) => {
+  if(req.session.user) return res.redirect('/')
   return res.render('login')
 }
 
-exports.login = (req, res, next) => {
-  return res.render('login')
+exports.login = async(req, res, next) => {
+  try {
+    const login = new Login(req.body)
+    await login.login()
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors)
+      return req.session.save(() => {
+        return res.redirect('/login')
+      })
+    }
+
+    req.session.user = login.user
+    req.flash('success', 'Login efetuado com sucesso!')
+    return req.session.save(() => {
+      return res.redirect('/')
+    })
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+exports.logout = (req, res, next) => {
+  req.session.destroy()
+  return res.redirect('/login')
 }
