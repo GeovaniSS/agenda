2b5e34d65a6bf34a04fff94bb716f4a735d43767
GeovaniSS <geosilvasousa@gmail.com>
2022-09-11 19:44:41 -0300

    [feat]: forms validations and hide flash messages

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,27 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import './assets/css/style.css';
+
+import Contact from './modules/Contact' 
+import Login from './modules/Login';
+import Register from './modules/Register'
+
+const alerts = document.querySelectorAll('.alert')
+
+setTimeout(() => {
+  alerts.forEach(alert =>  { 
+    alert.style.display = 'none'
+  })
+}, 2000)
+
+if(location.pathname.includes('register')) {
+  const register = new Register()
+}
+
+if(location.pathname.includes('login')) {
+  const login = new Login
+}
+
+if(location.pathname.includes('contact')) {
+  const contact = new Contact()
+}
