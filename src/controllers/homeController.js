a521189b513a662abcf55f55c9e83e00d0df22d7
GeovaniSS <geosilvasousa@gmail.com>
2022-09-10 22:16:26 -0300

    search for contacts in database and inject this in view index | make a loop in contacts using ejs and create a table row to all

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,15 @@
-exports.index = (req, res) => {
-  return res.render('index')
+const Contact = require('../models/ContactModel')
+
+exports.index = async(req, res) => {
+  try {
+    const contacts = await Contact.findContats()
+    return res.render('index', { contacts } )
+  } catch(e) {
+    console.log(e)
+    return res.render('404')
+  }
 } 
+
+// exports.filter = async(req, res) => {
+
+// }
